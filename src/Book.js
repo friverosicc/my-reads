e473e27e6f906620ff26073301dbc49a5bfc75d2
Francisco Riveros <friveros.icc@gmail.com>
2017-10-09 11:13:08 -0300

    feat: Get book list from the API

--- src/Book.js
+++ src/Book.js
@@ -2,15 +2,17 @@ import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 
 const Book = ({ book }) => {
+  const authors = book.authors.map(author => (<div>{author}</div>))
+
   return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.coverURL})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
           <BookShelfChanger />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.author}</div>
+        <div className="book-authors">{authors}</div>
       </div>
     </li>
   )
--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf'
 
-const Main = ({ booksReading, booksToRead, booksRead }) => {
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-          <BookShelf key="reading" title="Currently Reading" books={booksReading} />
-          <BookShelf key="to-read" title="Want to Read" books={booksToRead} />
-          <BookShelf key="read" title="Read" books={booksRead} />
+class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { books: [] }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(books => this.setState({ books }))
+  }
+
+
+  render() {
+    const shelfTypes = {
+      currentlyReading: 'currentlyReading',
+      wantToRead: 'wantToRead',
+      read: 'read'
+    }
+
+    const booksCurrentlyReading = this.state.books.filter( book => book.shelf === shelfTypes.currentlyReading)
+    const booksWantToRead = this.state.books.filter( book => book.shelf === shelfTypes.wantToRead)
+    const booksRead = this.state.books.filter( book => book.shelf === shelfTypes.read)
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf key="reading" title="Currently Reading" books={booksCurrentlyReading} />
+            <BookShelf key="to-read" title="Want to Read" books={booksWantToRead} />
+            <BookShelf key="read" title="Read" books={booksRead} />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
-      <div className="open-search">
-        <Link to="/search">Add a book</Link>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Main
