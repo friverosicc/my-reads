02aa17374f2db75e64a19981360cc3efa56edbdc
Francisco Riveros <friveros.icc@gmail.com>
2017-10-15 10:38:32 -0300

    refactor: Convert the BookShelfChanger to a stateless functional component

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class BookShelfChanger extends Component {
-  constructor(props) {
-    super(props)
+const BookShelfChanger = (props) => {
+  const selected = props.shelfSelected ? props.shelfSelected : 'none'
 
-    this.state = { selected: props.shelfSelected ? props.shelfSelected : 'none' }
-  }
-
-  render() {
-    return (
-      <div className="book-shelf-changer">
-        <select defaultValue={this.state.selected} onChange={event => this.props.handleShelfChange(event.target.value)}>
-          <option value="none" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    )
-  }
+  return (
+    <div className="book-shelf-changer">
+      <select defaultValue={selected} onChange={event => props.handleShelfChange(event.target.value)}>
+        <option value="none" disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  )
 }
 
 export default BookShelfChanger
