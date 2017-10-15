28d16acbd8983709017e3885599301a5b96f6e53
Francisco Riveros <friveros.icc@gmail.com>
2017-10-15 10:37:18 -0300

    refactor: Remove the use of state in the Book component

--- src/Book.js
+++ src/Book.js
@@ -5,27 +5,26 @@ class Book extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { book: props.book }
     this.handleShelfChange = this.handleShelfChange.bind(this)
   }
 
   handleShelfChange(shelfSelected) {
-    this.props.moveBookToAnotherShelf(this.state.book, shelfSelected)
+    this.props.moveBookToAnotherShelf(this.props.book, shelfSelected)
   }
 
   render() {
-    let authors = this.state.book.authors
+    let authors = this.props.book.authors
     authors = (authors) ? authors.map(author => (<div key={author}>{author}</div>)) : []
-    const coverImage = this.state.book.imageLinks.thumbnail
+    const coverImage = this.props.book.imageLinks.thumbnail
 
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ backgroundImage: `url(${coverImage})` }}></div>
-            <BookShelfChanger shelfSelected={this.state.book.shelf} handleShelfChange={this.handleShelfChange} />
+            <BookShelfChanger shelfSelected={this.props.book.shelf} handleShelfChange={this.handleShelfChange} />
           </div>
-          <div className="book-title">{this.state.book.title}</div>
+          <div className="book-title">{this.props.book.title}</div>
           <div className="book-authors">{authors}</div>
         </div>
       </li>
